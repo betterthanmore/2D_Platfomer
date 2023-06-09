75cf36388d2c63ae3f96dff2b75610246c48cb1c
junhee4613 <junhee000123@naver.com>
2023-06-10 00:51:24 +0900

    이동기 및 부스트 구현은 마무리 입력 값은 나중에 변경

--- EARTH/Assets/script/MainEarth/SubPlayer.cs
+++ EARTH/Assets/script/MainEarth/SubPlayer.cs
@@ -5,12 +5,15 @@ using UnityEngine.UI;
 
 public class SubPlayer : PlayerMainController
 {
-    public float boostDistanceLimit = 1;
-    public Scrollbar scr;
+    public Scrollbar scrollbar;
     public int boostTime = 0;           //부스트 시간
-    public float boostDistance = 0;     //부스트로 한번에 올라갈 수 있는 거리
-    private float subplayerPosY;    //부스트 사용할 때의 플레이어의 위치 저장
+    public float boostDistanceLimit = 1;//부스트로 한번에 올라갈 수 있는 거리
+    public Camera cm;
+    private float subplayerPosYCrt;    //부스트 사용할 때의 플레이어의 현재 위치 저장
+    private float subPlayerPosYTrs;     //부스트 사용하면서 바뀌는 위치Y 값을 저장
     private bool enableBoost = true;            //부스트 사용 가능 여부
+    private bool maxDistance = true;            //부스트로 올라간 거리가 최대치가 됐을 때 자동으로 떨어지게
+
     public override void Start()
     {
         base.Start();
@@ -20,24 +23,33 @@ public class SubPlayer : PlayerMainController
     public override void Update()
     {
         base.Update();
+        subPlayerPosYTrs = gameObject.transform.position.y;
         if (Input.GetKeyDown(KeyCode.W) && enableBoost)     //땅에 닿고 부스트 키를 눌렀을 때만 플레이어의 Y축 값을 저장한다.
         {
-            subplayerPosY = gameObject.transform.position.y;
+            subplayerPosYCrt = gameObject.transform.position.y;
         }
-        if (gameObject.tag == "SubPlayer" && Input.GetKey(KeyCode.W) && rb.velocity.y <= boostDistanceLimit && scr.value > 0.001 
-            && enableBoost /*여기에 플레이어가 올라갈 수 있는 거리에 제한 두기*/)        //부스트
+        
+        if (gameObject.tag == "SubPlayer" && Input.GetKey(KeyCode.W) && scrollbar.size > 0.001 && maxDistance)        //부스트
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            scr.value -= 1 * Time.fixedDeltaTime /boostTime;
+            
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            scrollbar.size -= 1 * Time.deltaTime /boostTime;
+            if (subPlayerPosYTrs >= subplayerPosYCrt + boostDistanceLimit)
+            {
+                Debug.Log(subplayerPosYCrt);
+                maxDistance = false;
+            }
         }
         if (isGround)   //땅에 닿는다면
         {
             enableBoost = true;
+            maxDistance = true;
         }
         else           //땅에서 떨어진다면
         {
             enableBoost = false;
         }
-        
+        scrollbar.transform.position = cm.WorldToScreenPoint(new Vector2(transform.position.x, transform.position.y + 1.5f));
+
     }
 }
