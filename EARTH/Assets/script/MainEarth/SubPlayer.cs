0d09b83c62cf2633dead1eeb62f408755f31772a
junhee4613 <junhee000123@naver.com>
2023-06-09 11:23:36 +0900

    서브플레이어 부스트 제작중

--- EARTH/Assets/script/MainEarth/SubPlayer.cs
+++ EARTH/Assets/script/MainEarth/SubPlayer.cs
@@ -9,7 +9,7 @@ public class SubPlayer : PlayerMainController
     public Scrollbar scr;
     public int boostTime = 0;           //부스트 시간
     public float boostDistance = 0;     //부스트로 한번에 올라갈 수 있는 거리
-    private float subplayerPosY = 0;    //부스트 사용할 때의 플레이어의 위치 저장
+    private float subplayerPosY;    //부스트 사용할 때의 플레이어의 위치 저장
     private bool enableBoost = true;            //부스트 사용 가능 여부
     public override void Start()
     {
@@ -20,11 +20,12 @@ public class SubPlayer : PlayerMainController
     public override void Update()
     {
         base.Update();
-        if (Input.GetKeyDown(KeyCode.W) && enableBoost)     //부스트 키를 눌렀을 때
+        if (Input.GetKeyDown(KeyCode.W) && enableBoost)     //땅에 닿고 부스트 키를 눌렀을 때만 플레이어의 Y축 값을 저장한다.
         {
             subplayerPosY = gameObject.transform.position.y;
         }
-        if (gameObject.tag == "SubPlayer" && Input.GetKey(KeyCode.W) && rb.velocity.y <= boostDistanceLimit && scr.value > 0.001 && enableBoost)        //부스트
+        if (gameObject.tag == "SubPlayer" && Input.GetKey(KeyCode.W) && rb.velocity.y <= boostDistanceLimit && scr.value > 0.001 
+            && enableBoost /*여기에 플레이어가 올라갈 수 있는 거리에 제한 두기*/)        //부스트
         {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             scr.value -= 1 * Time.fixedDeltaTime /boostTime;
